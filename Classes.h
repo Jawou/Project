db1fff4ab17b484d8bbb4c4b5750f52179e564e5
Durpy445 <durp446@gmail.com>
2025-06-03 14:02:00 +0100

    trianges;addedn

--- Classes.h
+++ Classes.h
@@ -11,10 +11,12 @@ public:
     double Magnitude;
 
     void Add(Vector3 NewVector)
-    {
+    {   
+
         this->X += NewVector.X;
         this->Y += NewVector.Y;
         this->Z += NewVector.Z;
+
         Update();
     }
     void Subtract(Vector3 NewVector)
@@ -99,13 +101,19 @@ public:
         this->X = Vector.X;
         this->Y = Vector.Y;
         this->Z = Vector.Z;
-        UpdatePostition();
+        UpdateVector();
     }
     //Adds Vector to CFrame (non directional)
-    void AddPos(Vector3 Vector) {
-        this->Vector.Add(Vector);
-        UpdatePostition();
+    void AddPos(Vector3 NewVector) {
+        this->Vector.Add(NewVector);
+        UpdateBaseValues();
     }
+    void UpdateBaseValues() {
+        this->X = Vector.X;
+        this->Y = Vector.Y;
+        this->Z = Vector.Z;
+    }
+        
     //Turns the euler angles stored into a rotation matrix set in the Za Xb Yg formation
     void UpdateMatrix() {
         double ca = cos(Alpha), sa = sin(Alpha);
@@ -128,10 +136,10 @@ public:
     //Updates All Values 
     void UpdateAll() {
         UpdateRotation();
-        UpdatePostition();
+        UpdateVector();
     }
     //Updates Only Position Related ELements
-    void UpdatePostition() {
+    void UpdateVector() {
         this->Vector = Vector3(X, Y, Z);
     }
     //Updates Only Rotation Related ELements
@@ -144,11 +152,10 @@ class Color3 {
 public:
     int RGB[3];
 
-    int Red;
-    int Green;
-    int Blue;
+    int Red = 255;
+    int Green = 255;
+    int Blue = 255;
 
-    Color3() {}
     Color3(int R, int G, int B) {
         this->RGB[0] = R;
         this->RGB[1] = G;
@@ -163,12 +170,93 @@ public:
 
 
 class Object {
-public:
-    char Name[100] = "Object"; //Max 99 characters due to the null at the end of a char array
-    float Transparency = 0; //Range 1-0
-    Color3 Color = Color3(255, 255, 255);
-    Vector3 Size = Vector3(5, 5, 5);
-    CFrame Position = CFrame(0, 0, 0);
-    char Shape[100] = "Cube";
-
+    public:
+        char Name[100] = "Object"; //Max 99 characters due to the null at the end of a char array
+        float Transparency = 0; //Range 1-0
+        Color3 Color = Color3(255, 255, 255);
+        Vector3 Size = Vector3(5, 5, 5);
+        CFrame Position = CFrame(0, 0, 0);
+        Vector3 RelativeVertices[99]; //store in relation to the object
+        CFrame AbsoluteVertices[99]; //store in relation to the object
+        Vector3 Triangles[99][3];
 };
+
+class Cube : public Object {
+    public:
+        void CreateRelVert(){
+            this->RelativeVertices[0] = Vector3(Size.X / 2,Size.Y/2, Size.Z / 2); //+++
+            this->RelativeVertices[1] = Vector3(Size.X / 2, Size.Y / 2, -Size.Z / 2);//++-
+            this->RelativeVertices[2] = Vector3(Size.X / 2, -Size.Y / 2, Size.Z / 2);//+-+
+            this->RelativeVertices[3] = Vector3(Size.X / 2, -Size.Y / 2, -Size.Z / 2);//+--
+            this->RelativeVertices[4] = Vector3(-Size.X / 2, Size.Y / 2, Size.Z / 2);//-++
+            this->RelativeVertices[5] = Vector3(-Size.X / 2, Size.Y / 2, -Size.Z / 2);//-+-
+            this->RelativeVertices[6] = Vector3(-Size.X / 2, -Size.Y / 2, Size.Z / 2);//--+
+            this->RelativeVertices[7] = Vector3(-Size.X / 2, -Size.Y / 2, -Size.Z / 2);//---
+            
+            
+            CreateAbsVert();
+        }
+        void CreateTriangles(){
+            Triangles[0][0] = RelativeVertices[0];
+            Triangles[0][1] = RelativeVertices[2];
+            Triangles[0][2] = RelativeVertices[4];
+
+            Triangles[1][0] = RelativeVertices[4];
+            Triangles[1][1] = RelativeVertices[2];
+            Triangles[1][2] = RelativeVertices[6];
+
+            Triangles[2][0] = RelativeVertices[1];
+            Triangles[2][1] = RelativeVertices[5];
+            Triangles[2][2] = RelativeVertices[3];
+
+            Triangles[3][0] = RelativeVertices[3];
+            Triangles[3][1] = RelativeVertices[5];
+            Triangles[3][2] = RelativeVertices[7];
+
+            Triangles[4][0] = RelativeVertices[0];
+            Triangles[4][1] = RelativeVertices[4];
+            Triangles[4][2] = RelativeVertices[1];
+
+            Triangles[5][0] = RelativeVertices[1];
+            Triangles[5][1] = RelativeVertices[4];
+            Triangles[5][2] = RelativeVertices[5];
+
+            Triangles[6][0] = RelativeVertices[2];
+            Triangles[6][1] = RelativeVertices[3];
+            Triangles[6][2] = RelativeVertices[6];
+
+            Triangles[7][0] = RelativeVertices[6];
+            Triangles[7][1] = RelativeVertices[3];
+            Triangles[7][2] = RelativeVertices[7];
+
+            Triangles[8][0] = RelativeVertices[0];
+            Triangles[8][1] = RelativeVertices[1];
+            Triangles[8][2] = RelativeVertices[2];
+
+            Triangles[9][0] = RelativeVertices[2];
+            Triangles[9][1] = RelativeVertices[1];
+            Triangles[9][2] = RelativeVertices[3];
+
+            Triangles[10][0] = RelativeVertices[4];
+            Triangles[10][1] = RelativeVertices[6];
+            Triangles[10][2] = RelativeVertices[5];
+
+            Triangles[11][0] = RelativeVertices[5];
+            Triangles[11][1] = RelativeVertices[6];
+            Triangles[11][2] = RelativeVertices[7];
+
+
+        }
+        void CreateAbsVert() {
+            for (int i = 0; i < 8; i++){
+                CFrame NewFrame(Position);
+                NewFrame.AddPos(RelativeVertices[i]);
+                this->AbsoluteVertices[i] = NewFrame;
+            };
+        }
+
+        void ApplyRotation() {
+
+        }
+};
+
--- Main.cpp
+++ Main.cpp
@@ -6,13 +6,19 @@
 
 int main()
 {
-    Object Test;
+    Cube Test;
     Test.Color = Color3(0, 125, 125);
     Test.Size = Vector3(5, 5, 5);
-    Test.Position = CFrame(0, 0, 0);
+    Test.Position = CFrame(125, 0, 0);
+
+    Test.CreateRelVert();
+    Test.CreateTriangles();
+    for (int i = 0; i < 12; i++)
+    {
+        printf("triangle((%f,%f,%f),(%f,%f,%f),(%f,%f,%f))", Test.Triangles[i][0].X, Test.Triangles[i][0].Y, Test.Triangles[i][0].Z, Test.Triangles[i][1].X, Test.Triangles[i][1].Y, Test.Triangles[i][1].Z, Test.Triangles[i][2].X, Test.Triangles[i][2].Y, Test.Triangles[i][2].Z);
+    }
 
 
     printf("\n%f", Test.Size.X);
-    printf("\n%s", Test.Shape);
     return 0;
 }
